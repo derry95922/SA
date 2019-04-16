53350bffa6f6fb3e4a189053225c67b1b964066b
fourbags <derry95922@gmail.com>
2019-04-16 23:09:41 +0800

    4/16finish crud ut

--- sa_hw/app/src/androidTest/java/com/example/sa_hw/UseCaseTest.java
+++ sa_hw/app/src/androidTest/java/com/example/sa_hw/UseCaseTest.java
@@ -49,7 +49,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class UseCaseTest {
 
     Context appContext = InstrumentationRegistry.getTargetContext();
     CourseRepository courseRepository = new CourseRepositoryImpl(appContext);
